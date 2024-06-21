a6e051c48ca06827132f30ff6a26277a63a00234
Ardhi <92952103+dna-ardhi@users.noreply.github.com>
2024-06-21 22:13:01 +0800

    config postcss

---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <nav>
+      <NuxtLink to="#">Menu</NuxtLink>
+      <NuxtLink to="/">Logo</NuxtLink>
+      <NuxtLink to="#contact">Get in Touch</NuxtLink>
+    </nav>
+  </header>
+</template>
