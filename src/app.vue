6d2c6ee173b20489ffac3fdb56a4e91636784c8d
Ardhi <92952103+dna-ardhi@users.noreply.github.com>
2024-06-26 14:29:17 +0800

    feat: add config for tag head

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'bg-gray-900 text-white'
+  },
+  script: [{ innerHTML: 'console.log(\'Hello world\')' }]
+})
+</script>
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
