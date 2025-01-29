f130eb3383a9394414e419853010383a8f418596
Ardhi <92952103+dna-ardhi@users.noreply.github.com>
2025-01-29 23:37:04 +0800

    fix: adjust font-size and margin for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,15 +19,15 @@ onUnmounted(() => {
     class="text-white fixed top-0 z-10 w-full transition-all duration-500 ease-in-out"
     :class="{ 'backdrop-brightness-75 backdrop-blur-lg': isScrolling }"
   >
-    <nav class="container mx-auto flex justify-between items-center px-8 lg:px-0 py-3">
+    <nav class="container mx-auto flex justify-between items-center px-4 lg:px-0 py-3">
       <article class="flex-1 text-left">
-        <NuxtLink class="text-xl" to="#">Menu</NuxtLink>
+        <NuxtLink class="text-sm md:text-xl" to="#">Menu</NuxtLink>
       </article>
       <article class="flex-1 text-center">
         <NuxtLink class="text-3xl uppercase" to="/">Logo</NuxtLink>
       </article>
       <article class="flex-1 text-right">
-        <NuxtLink class="text-xl" to="#contact">Get in Touch</NuxtLink>
+        <NuxtLink class="text-sm md:text-xl" to="#contact">Get in Touch</NuxtLink>
       </article>
     </nav>
   </header>
