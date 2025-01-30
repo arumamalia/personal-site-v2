9f7516ca4e399b73421d10d1360b66d97aaf02c5
Ardhi <dardhi@pmintl.net>
2025-01-30 21:18:01 +0800

    fix: add attribute mdc-unwrap="p"

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,20 +5,20 @@
   <section class="flex flex-col min-h-screen py-10">
     <article class="flex flex-auto justify-center items-center py-32 w-full">
       <h1 class="text-5xl text-center">
-        <slot />
+        <slot mdc-unwrap="p" />
       </h1>
     </article>
     <article class="flex flex-initial flex-wrap gap-8">
       <header class="basis-sm">
         <h2 class="text-2xl">
-          <slot name="title" />
+          <slot name="title" mdc-unwrap="p" />
         </h2>
         <h3 class="text-3xl">
-          <slot name="subtitle" />
+          <slot name="subtitle" mdc-unwrap="p" />
         </h3>
       </header>
       <p class="basis-sm grow text-base text-justify">
-        <slot name="description" />
+        <slot name="description" mdc-unwrap="p" />
       </p>
     </article>
   </section>
--- content/hero.md
+++ content/hero.md
@@ -5,13 +5,13 @@ description: 'content for hero section'
 
 ::Hero
 
-# Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
 
 #title
-## About Us
+About Us
 
 #subtitle
-### Hanacaraka
+Hanacaraka
 
 #description
 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi perspiciatis rerum pariatur repudiandae cum nihil possimus velit, illo ut itaque voluptate adipisci qui fuga, quia laboriosam sequi sapiente placeat suscipit voluptates provident quae? Laborum itaque sequi, magnam deserunt, facilis quam amet nulla accusantium saepe provident eligendi maiores, minima repudiandae harum.
