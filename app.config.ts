854b1e7d4b747a19dd382c78b0955c0c2befbecd
Dadi Nur Ardhi <92952103+dna-ardhi@users.noreply.github.com>
2025-01-30 20:21:36 +0800

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,9 @@
-export default defineAppConfig({})
+export default defineAppConfig({
+  global: {
+    general: {
+      site_name: "Hanacaraka",
+      site_description: "Selamat Datang di Welcome :)",
+      favicon: "i-material-symbols-coffee"
+    }
+  }
+})
