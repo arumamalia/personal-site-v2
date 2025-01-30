abcc24d5304283e3217f97dcaa65e58df5b4be2b
Ardhi <dardhi@pmintl.net>
2025-01-30 16:16:50 +0800

    deps: add nuxt studio package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "nuxt-swiper", "@vesp/nuxt-fontawesome"],
+  modules: [
+    "@nuxthq/studio",
+    "@nuxt/content",
+    "nuxt-swiper",
+    "@vesp/nuxt-fontawesome",
+  ],
   css: [
     "@fontsource/poppins/400.css",
     "@fontsource/poppins/500.css",
