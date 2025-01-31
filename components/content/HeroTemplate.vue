759732a18d8a195e61e0f67bd5c610b7a05db7bb
Ardhi <dardhi@pmintl.net>
2025-01-31 23:58:02 +0800

    fix: downgrade nuxt content to v2

--- components/content/HeroTemplate.vue
+++ components/content/HeroTemplate.vue
@@ -1,24 +1,28 @@
-
 <script setup lang="ts">
+  defineSlots<{
+    title: () => string,
+    subtitle: () => string,
+    description: () => string,
+  }>();
 </script>
 <template>
   <section class="flex flex-col min-h-screen py-10">
     <article class="flex flex-auto justify-center items-center py-32 w-full">
       <h1 class="text-5xl text-center">
-        <slot mdc-unwrap="p" />
+        <ContentSlot unwrap="p" />
       </h1>
     </article>
     <article class="flex flex-initial flex-wrap gap-8">
       <header class="basis-sm">
         <h2 class="text-2xl">
-          <slot name="title" mdc-unwrap="p" />
+          <ContentSlot :use="$slots.title" unwrap="p" />
         </h2>
         <h3 class="text-3xl">
-          <slot name="subtitle" mdc-unwrap="p" />
+          <ContentSlot :use="$slots.subtitle" unwrap="p" />
         </h3>
       </header>
       <p class="basis-sm grow text-base text-justify">
-        <slot name="description" mdc-unwrap="p" />
+        <ContentSlot :use="$slots.description" unwrap="p" />
       </p>
     </article>
   </section>
--- content/home.md
+++ content/home.md
@@ -1,12 +1,13 @@
 
-::hero-section
-::
+::hero-template
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
 
-::our-service-section
-::
+#title
+About Us
 
-::project-section
-::
+#subtitle
+Hanacaraka-
 
-::contact-section
+#description
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi perspiciatis rerum pariatur repudiandae cum nihil possimus velit, illo ut itaque voluptate adipisci qui fuga, quia laboriosam sequi sapiente placeat suscipit voluptates provident quae? Laborum itaque sequi, magnam deserunt, facilis quam amet nulla accusantium saepe provident eligendi maiores, minima repudiandae harum.
 ::
