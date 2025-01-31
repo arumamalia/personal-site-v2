9e38eb03d2bb29befad49463559ba07bff0c63d2
Ardhi <dardhi@pmintl.net>
2025-02-01 01:24:44 +0800

    fix: always return error

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
   const route = useRoute();
-  const { data: page } = useAsyncData('content-' + route.path, () => queryContent(route.path).findOne());
+  const { error } = useAsyncData('content-' + route.path, () => queryContent(route.path).findOne());
 
-  if (!page.value) {
+  if (error.value) {
     throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
   }
 </script>
 <template>
-  <ContentDoc />
+  <ContentDoc v-if="!error" />
 </template>
