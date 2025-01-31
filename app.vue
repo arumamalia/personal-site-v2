6f338658c4ecff9b2191a17a51f208994449a1f5
Ardhi <dardhi@pmintl.net>
2025-02-01 00:19:30 +0800

    feat: add dynamic favicon based on app config

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const appConfig = useAppConfig();
-const { site_name, site_description } = appConfig?.global?.general || {};
+const { site_name, site_description, favicon } = appConfig?.global?.general || {};
 useHead({
   title: site_name,
   meta: [
@@ -9,6 +9,9 @@ useHead({
   bodyAttrs: {
     class: 'bg-gray-900 text-white',
   },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: favicon },
+  ]
 })
 </script>
 <template>
