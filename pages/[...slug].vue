10daa1c0b7351c720ebad8c779248e3eaa2f9e0c
Ardhi <dardhi@pmintl.net>
2025-02-01 02:32:51 +0800

    fix: add await syntax while useAsyncData

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   const route = useRoute();
-  const { error } = useAsyncData('content-' + route.path, () => queryContent(route.path).findOne());
+  const { error } = await useAsyncData('content-' + route.path, () => queryContent(route.path).findOne());
 
   if (error.value) {
     throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
