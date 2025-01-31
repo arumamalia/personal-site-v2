945081fc32dc8819b73b6afb55468cb44edc620c
Ardhi <dardhi@pmintl.net>
2025-02-01 02:31:51 +0800

    feat: intagrate project section to nuxt content

--- components/content/ProjectTemplate.client.vue
+++ components/content/ProjectTemplate.client.vue
@@ -1,20 +1,24 @@
 <script lang="ts" setup>
-const slides = ref(Array.from({ length: 10 }, () => {
-  const r = Math.floor(Math.random() * 256)
-  const g = Math.floor(Math.random() * 256)
-  const b = Math.floor(Math.random() * 256)
-  // Figure out contrast color for font
-  const contrast = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? 'black' : 'white'
+  interface Project {
+    id: string
+    image: string
+    title?: string
+  }
 
-  return { bg: `rgb(${r}, ${g}, ${b})`, color: contrast }
-}))
+  interface ProjectData {
+    body: Project[] | null,
+  }
+
+  const { data } = await useAsyncData<ProjectData>('fetch-projects', async () => {
+    return await queryContent('/json/projects').findOne() as ProjectData;
+  });
 </script>
 
 <template>
   <section class="h-200 flex flex-col justify-center gap-8">
     <h2 class="text-center text-3xl">Projects</h2>
     <article class="flex gap-4">
-      <button id="prev-project" class="text-3xl shrink-0">
+      <button id="prev-project" class="text-3xl shrink-0 cursor-pointer">
         <font-awesome icon="chevron-left" />
       </button>
       <div class="basis-auto grow-0 w-full overflow-hidden">
@@ -45,15 +49,12 @@ const slides = ref(Array.from({ length: 10 }, () => {
           }" 
           class="w-full"
         >
-          <SwiperSlide 
-            v-for="(slide, idx) in slides" :key="idx"
-            :style="`background-color: ${slide.bg}; color: ${slide.color}`"
-          >
-            {{ idx }}
+          <SwiperSlide v-for="slide in data?.body ?? []" :key="slide?.id" >
+            <img :src="slide?.image" alt="">
           </SwiperSlide>
         </Swiper>
       </div>
-      <button id="next-project" class="text-3xl shrink-0">
+      <button id="next-project" class="text-3xl shrink-0 cursor-pointer">
         <font-awesome icon="chevron-right" />
       </button>
     </article>
@@ -61,14 +62,24 @@ const slides = ref(Array.from({ length: 10 }, () => {
 </template>
 
 <style>
-.swiper-slide {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 295px;
-  font-size: 4rem;
-  font-weight: bold;
-  border-radius: 12px;
-}
+  .swiper-wrapper {
+    height: 200px;
+  } 
+
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200;
+    font-size: 4rem;
+    font-weight: bold;
+    border-radius: 12px;
+  }
+
+  .swiper-slide img {
+    width: 100%;
+    height: 100% !important;
+    object-fit: cover;
+  }
 </style>
 
--- content/home.md
+++ content/home.md
@@ -1,10 +1,20 @@
-::hero-section
+::hero-template
+Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+
+#title
+About Us
+
+#subtitle
+Hanacaraka.
+
+#description
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi perspiciatis rerum pariatur repudiandae cum nihil possimus velit, illo ut itaque voluptate adipisci qui fuga, quia laboriosam sequi sapiente placeat suscipit voluptates provident quae? Laborum itaque sequi, magnam deserunt, facilis quam amet nulla accusantium saepe provident eligendi maiores, minima repudiandae harum.
 ::
 
 ::our-service-section
 ::
 
-::project-section
+::project-template 
 ::
 
 ::contact-section
