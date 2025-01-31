155d62442a42ba6d8032964c753b0e39e2266669
Ardhi <dardhi@pmintl.net>
2025-02-01 01:09:03 +0800

    feat: add handle page not found

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+  const route = useRoute();
+  const { data: page } = useAsyncData('content-' + route.path, () => queryContent(route.path).findOne());
+
+  if (!page.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+  }
+</script>
 <template>
   <ContentDoc />
 </template>
