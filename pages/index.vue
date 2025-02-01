9c597cab5bfad55a92c25b7747eef91c63e56881
Ardhi <dardhi@pmintl.net>
2025-02-01 14:24:42 +0800

    fix: switch static landing page with dynamic one

--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <HeroSection />
-  <ServiceTemplate>Services</ServiceTemplate>
-  <ProjectTemplate>Portfolio</ProjectTemplate>
-  <ContactTemplate>Get in Touch</ContactTemplate>
-</template>
