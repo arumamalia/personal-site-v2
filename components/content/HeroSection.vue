1487e2b9141c083872a8627950968bc002ce4698
Ardhi <dardhi@pmintl.net>
2025-02-01 23:55:27 +0800

    fix: adjust min height hero section

--- components/content/HeroSection.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-const { data: page } = await useAsyncData('content-section-hero', () => {
-  return queryContent('/hero').findOne();
-});
-</script>
-<template>
-  <ContentRenderer v-if="page" :value="page" />
-</template>
